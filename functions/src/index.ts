72868e3e3eb597a1db7ba9707b3f4c29a52b9b7b
junhak <keea9@naver.com>
2020-01-30 15:35:38 +0900

    release branch 생성 및 코드 수정

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,10 @@
 import * as admin from "firebase-admin";
 
 // release branch
-// var serviceAccount = require("../gubuapp-firebase-adminsdk-ache6-d79a8a8fa0.json");
+var serviceAccount = require("../gubuapp-firebase-adminsdk-ache6-d79a8a8fa0.json");
 
 // master branch
-var serviceAccount = require("../service-account.json");
+// var serviceAccount = require("../service-account.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
